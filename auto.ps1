5fa3798ffd66d04556fa076afb24a1014f7b5063
cth.release <84012697+th-release@users.noreply.github.com>
2023-06-23 06:17:31 +0900

    Update auto.ps1

--- auto.ps1
+++ auto.ps1
@@ -3,10 +3,9 @@
 # Variables
 $RESOURCE_GROUP_NAME = "rg-hg-httpsgithubcomhackersground-krazure-kawaii"
 $WEBAPP_NAME_BACKEND = "pyl-webapp-backend-prod"
-$WEBAPP_NAME_FRONTEND = "pyl-webapp-frontend-pord"
 $LOCATION = "koreacentral"
 $APP_SERVICE_PLAN = "pyl-plan"
-$MYSQL_SERVER_NAME = "pyl-database-prod"
+$MYSQL_SERVER_NAME = "pyl-database-mysql"
 $MYSQL_USERNAME = "pyl"
 $MYSQL_PASSWORD = "Password1234"
 $NODE_VERSION = "16"
@@ -32,30 +31,23 @@ az mysql flexible-server db create --resource-group $RESOURCE_GROUP_NAME --serve
 $MYSQL_SERVER_DNS=az mysql flexible-server show --resource-group $RESOURCE_GROUP_NAME --name $MYSQL_SERVER_NAME --query "fullyQualifiedDomainName" --output tsv
 # Create a Node.js web app
 az webapp create --name $WEBAPP_NAME_BACKEND --plan $APP_SERVICE_PLAN --resource-group $RESOURCE_GROUP_NAME --runtime "$NODE_RUNTIME"
-az webapp create --name $WEBAPP_NAME_FRONTEND --plan $APP_SERVICE_PLAN --resource-group $RESOURCE_GROUP_NAME --runtime "$NODE_RUNTIME"
 
 # Configure the Node.js version
 az webapp config appsettings set --name $WEBAPP_NAME_BACKEND --resource-group $RESOURCE_GROUP_NAME --settings WEBSITE_NODE_DEFAULT_VERSION=$NODE_VERSION
-az webapp config appsettings set --name $WEBAPP_NAME_FRONTEND --resource-group $RESOURCE_GROUP_NAME --settings WEBSITE_NODE_DEFAULT_VERSION=$NODE_VERSION
 
-az webapp config set --name $WEBAPP_NAME_BACKEND --resource-group $RESOURCE_GROUP_NAME --startup-file "node src/."  # 웹 앱의 시작 명령을 "npm start"로 설정
-az webapp config set --name $WEBAPP_NAME_FRONTEND --resource-group $RESOURCE_GROUP_NAME --startup-file "cd ./frontend && npm run dev"  # 웹 앱의 시작 명령을 "npm start"로 설정
+az webapp config set --name $WEBAPP_NAME_BACKEND --resource-group $RESOURCE_GROUP_NAME --startup-file "npm start"  # 웹 앱의 시작 명령을 "npm start"로 설정
                                                                                                   
 az webapp deployment list-publishing-profiles --name $WEBAPP_NAME_BACKEND --resource-group $RESOURCE_GROUP_NAME --xml > back_publish_profile.xml
-az webapp deployment list-publishing-profiles --name $WEBAPP_NAME_FRONTEND --resource-group $RESOURCE_GROUP_NAME --xml > front_publish_profile.xml
 
 gh secret set BACKEND_APP_NAME --repo $GITHUB_USERNAME/$GITHUB_REPOSITORY --body $WEBAPP_NAME_BACKEND
-gh secret set FRONTEND_APP_NAME --repo $GITHUB_USERNAME/$GITHUB_REPOSITORY --body $WEBAPP_NAME_FRONTEND
 
-gh secret set MYSQL_SERVER_DNS --repo $GITHUB_USERNAME/$GITHUB_REPOSITORY --body $MYSQL_SERVER_DNS
+q/$GITHUB_REPOSITORY --body $MYSQL_SERVER_DNS
 gh secret set MYSQL_USERNAME --repo $GITHUB_USERNAME/$GITHUB_REPOSITORY --body $MYSQL_USERNAME
 gh secret set MYSQL_PASSWORD --repo $GITHUB_USERNAME/$GITHUB_REPOSITORY --body $MYSQL_PASSWORD
 gh secret set MYSQL_DBNAME --repo $GITHUB_USERNAME/$GITHUB_REPOSITORY --body $DB_NAME
 
 cat ./back_publish_profile.xml | gh secret set BACKEND_WEBAPP_PUBLISH_PROFILE --repo $GITHUB_USERNAME/$GITHUB_REPOSITORY
-cat ./front_publish_profile.xml | gh secret set FRONTEND_WEBAPP_PUBLISH_PROFILE --repo $GITHUB_USERNAME/$GITHUB_REPOSITORY
 
 gh auth login
 
 gh workflow run "Backend Deploy" --repo $GITHUB_USERNAME/$GITHUB_REPOSITORY
-gh workflow run "Front Deploy" --repo $GITHUB_USERNAME/$GITHUB_REPOSITORY
