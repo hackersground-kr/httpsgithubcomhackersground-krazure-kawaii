cd7ac0e785d882a8dfbd893a2b2c45f7c0523803
cth.release <84012697+th-release@users.noreply.github.com>
2023-06-23 07:17:46 +0900

    Update setup.js

--- backend/setup.js
+++ backend/setup.js
@@ -12,29 +12,28 @@ const sleep = (ms) => {
 
 async function check(directoryPath) {
   console.log(await db.raw('show tables'))
-  await db.insert({ uuid: v4(), name: 'it' }).into('categoryEntity')
-  // fs.readdir(directoryPath, async (err, files) => {
-  //   if (err) {
-  //     console.error('디렉토리를 읽을 수 없습니다:', err);
-  //     return;
-  //   }
+  fs.readdir(directoryPath, async (err, files) => {
+    if (err) {
+      console.error('디렉토리를 읽을 수 없습니다:', err);
+      return;
+    }
 
-  //   sqlFiles.forEach(async (file) => {
-  //     if (file.endsWith('.sql')) {
-  //       const filePath = `${directoryPath}/${file}`;
-  //       console.log(`실행 중인 SQL 파일: ${filePath}`);
+    sqlFiles.forEach(async (file) => {
+      if (file.endsWith('.sql')) {
+        const filePath = `${directoryPath}/${file}`;
+        console.log(`실행 중인 SQL 파일: ${filePath}`);
         
-  //       const sql = fs.readFileSync(filePath, 'utf-8');
-  //       console.log(sql)
+        const sql = fs.readFileSync(filePath, 'utf-8');
+        console.log(sql)
         
-  //       await db.raw(sql);
-  //       sleep(1000 * 3)
-  //     }
-  //   });
-  // });
-  // setTimeout(() => {
-  //   process.exit(0);
-  // }, 1000*24)
+        await db.raw(sql);
+        sleep(1000 * 3)
+      }
+    });
+  });
+  setTimeout(() => {
+    process.exit(0);
+  }, 1000*24)
 }
 
 check(path.join(process.cwd() + '/src/sql'));
